f74e27edac3e207dcecdcbad9ab73cee628d7692
shivanichitta <146317671+shivanichitta@users.noreply.github.com>
2024-03-06 13:52:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    const menu=document.querySelector(".menu-links");
+    const menu=document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+    
+
+}
